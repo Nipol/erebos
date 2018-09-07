16fc390c17760357e19f5a2734350a8b5819fbf5
Paul Le Cam <paul@mainframe.com>
2018-09-07 14:26:28 +0100

    MRU digest utilities

--- packages/api-bzz-base/src/index.js
+++ packages/api-bzz-base/src/index.js
@@ -1,5 +1,28 @@
 // @flow
 
+import { keccak256 } from 'js-sha3'
+
+const BZZ_KEY_LENGTH = 32
+const MRU_META_HASH_LENGTH = BZZ_KEY_LENGTH
+const MRU_ROOT_ADDR_LENGTH = BZZ_KEY_LENGTH
+const MRU_UPDATE_VERSION_LENGTH = 4
+const MRU_UPDATE_PERIOD_LENGTH = 4
+const MRU_UPDATE_FLAG_LENGTH = 1
+const MRU_UPDATE_DATA_LENGTH_LENGTH = 2
+const MRU_UPDATE_HEADER_LENGTH_LENGTH = 2
+const MRU_UPDATE_HEADER_LENGTH =
+  MRU_META_HASH_LENGTH +
+  MRU_ROOT_ADDR_LENGTH +
+  MRU_UPDATE_VERSION_LENGTH +
+  MRU_UPDATE_PERIOD_LENGTH +
+  MRU_UPDATE_FLAG_LENGTH
+
+const BUFFER_WRITE_SIZE = {
+  1: 'writeUInt8',
+  2: 'writeUInt16LE',
+  4: 'writeUInt32LE',
+}
+
 export type DirectoryData = {
   [path: string]: { data: string | Buffer, contentType: string, size?: number },
 }
@@ -21,6 +44,59 @@ export type ListEntry = {
 export type ListResult = {
   common_prefixes?: Array<string>,
   entries?: Array<ListEntry>,
+}
+
+export type ResourceDigestParams = {
+  period: number,
+  version: number,
+  multihash: boolean,
+  data: Buffer | string,
+  metaHash: string,
+  rootAddr: string,
+}
+
+export const bufferFromHex = (value: string): Buffer => {
+  return Buffer.from(value.substr(2), 'hex')
+}
+
+export const createBuffer = (size: 1 | 2 | 4, value: number): Buffer => {
+  const writeFunc = BUFFER_WRITE_SIZE[size]
+  if (writeFunc == null) {
+    throw new Error('Invalid size')
+  }
+  const buffer = Buffer.allocUnsafe(size)
+  buffer[writeFunc](value, 0)
+  return buffer
+}
+
+const getBuffer = (size: number, value: string): Buffer => {
+  const buffer = bufferFromHex(value)
+  if (buffer.length !== size) {
+    throw new Error('Invalid input')
+  }
+  return buffer
+}
+
+const HEADER_LENGTH_BUFFER = createBuffer(
+  MRU_UPDATE_HEADER_LENGTH_LENGTH,
+  MRU_UPDATE_HEADER_LENGTH,
+)
+
+export const createResourceDigest = (params: ResourceDigestParams): string => {
+  const data = Buffer.isBuffer(params.data)
+    ? params.data
+    : bufferFromHex(params.data)
+  const payload = Buffer.concat([
+    HEADER_LENGTH_BUFFER,
+    createBuffer(MRU_UPDATE_DATA_LENGTH_LENGTH, data.length),
+    createBuffer(MRU_UPDATE_PERIOD_LENGTH, params.period),
+    createBuffer(MRU_UPDATE_VERSION_LENGTH, params.version),
+    getBuffer(MRU_ROOT_ADDR_LENGTH, params.rootAddr),
+    getBuffer(MRU_META_HASH_LENGTH, params.metaHash),
+    createBuffer(MRU_UPDATE_FLAG_LENGTH, params.multihash ? 1 : 0),
+    data,
+  ])
+  return '0x' + keccak256(payload)
 }
 
 export const resOrError = (res: *) => {
